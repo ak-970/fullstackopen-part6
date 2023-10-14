bd5ce304ac9da69c0827b614c87b42649964f0ee
ak-970 <antje.kasemann@gmail.com>
2023-10-14 19:05:00 +0200

    Part 6c: Anecdotes and the backend, step1 - step2

--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -1,6 +1,9 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 
+// import anecdoteService from './services/anecdotes'
+
+// import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
@@ -12,5 +15,9 @@ const store = configureStore({
     notification: notificationReducer
   }
 })
+
+// anecdoteService.getAll().then(anecdotes =>
+//   store.dispatch(setAnecdotes(anecdotes))
+// )
 
 export default store
